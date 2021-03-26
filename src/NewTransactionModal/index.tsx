28f2649ea2cc8fac234ba359f7242dd118207fa0
hyuri-mobile <hyuri@mobilesaude.com.br>
2021-03-26 10:20:39 -0300

    finalizado projeto

--- src/NewTransactionModal/index.tsx
+++ src/NewTransactionModal/index.tsx
@@ -3,7 +3,8 @@ import iconmeImg from '../assets/income.svg';
 import outcomeImg from '../assets/outcome.svg';
 import closeImg from '../assets/close.svg';
 import Modal from "react-modal";
-import { useState } from 'react';
+import { FormEvent, useState } from 'react';
+import { api } from '../services/api';
 
 interface NewTransactionModalPops {
     isOpen: boolean;
@@ -11,9 +12,24 @@ interface NewTransactionModalPops {
 }
 
 export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionModalPops){
-    const [type, setType] = useState('ganhos')
+  const [title, setTitle] = useState('');
+  const [value, setValue] = useState(0);
+  const [category, setCategory] = useState('');  
+  const [type, setType] = useState('ganhos');
 
+   function handleCreateNewTransactionModal(event: FormEvent){
+       event.preventDefault();
+
+       const data = {
+         title,
+         value,
+         category,
+         type
+       };
+
+       api.post('/transactions', data)
   
+    }
 
     return(
         <Modal 
@@ -34,10 +50,15 @@ export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionMo
            <h2>Cadastar</h2>
             <input
              placeholder="Titulo"
+             value={title}
+             onChange={event=>setTitle(event.target.value)}
             />
 
            <input
+             type="number"
              placeholder="Valor"
+             value={value}
+             onChange={event=>setValue(Number(event.target.value))}
             />
 
            <TransatctionTypeContainer>
@@ -64,6 +85,8 @@ export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionMo
 
            <input
              placeholder="Categoria"
+             value={category}
+             onChange={event=>setCategory(event.target.value)}
             />
 
         
